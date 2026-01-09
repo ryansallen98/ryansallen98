ab298fc06781f409957a2fdd9f5a97a69c2395de
Ryan Allen <ryansallen53@gmail.com>
2026-01-09 20:39:29 +0000

    feat: add astro-icon integration to Astro configuration

--- astro.config.ts
+++ astro.config.ts
@@ -5,11 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 import alpinejs from '@astrojs/alpinejs';
 
+import icon from 'astro-icon';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [alpinejs()]
+  integrations: [alpinejs(), icon()]
 });
