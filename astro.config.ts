604a8608a2dd2f339103e521af8a4df9ec0c38ee
Ryan Allen <ryansallen53@gmail.com>
2026-01-11 17:44:20 +0000

    feat: add YAML plugin for Rollup to support YAML file handling

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import alpinejs from "@astrojs/alpinejs";
-
 import icon from "astro-icon";
+import yaml from "@rollup/plugin-yaml";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss(), yaml()],
   },
 
   integrations: [
