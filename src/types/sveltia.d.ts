c81b52704d0a01945d33f72633d6d76a154eb0f3
Ryan Allen <ryansallen53@gmail.com>
2026-01-11 13:04:19 +0000

    feat: add CmsFile type to enhance type safety for CMS file handling

--- src/types/sveltia.d.ts
+++ src/types/sveltia.d.ts
@@ -3,4 +3,7 @@ import type { CmsConfig } from "@sveltia/cms";
 declare global {
   type CmsCollections = NonNullable<CmsConfig["collections"]>;
   type CmsCollection = Exclude<CmsCollections[number], { type: "divider" }>;
+  type CmsFile = NonNullable<
+    Extract<CmsCollection, { files: any }>["files"]
+  >[number];
 }
