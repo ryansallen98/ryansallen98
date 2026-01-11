9e24c53d9b03d69a02a588fbff960de8ce16bdd0
Ryan Allen <ryansallen53@gmail.com>
2026-01-11 22:42:57 +0000

    feat: add sitemap integration to enhance SEO capabilities

--- astro.config.ts
+++ astro.config.ts
@@ -4,15 +4,21 @@ import tailwindcss from "@tailwindcss/vite";
 import alpinejs from "@astrojs/alpinejs";
 import icon from "astro-icon";
 import yaml from "@rollup/plugin-yaml";
+import sitemap from "@astrojs/sitemap";
+import { loadEnv } from "vite";
+
+const env = loadEnv("", process.cwd(), "");
+const site = env.SITE_URL;
 
 // https://astro.build/config
 export default defineConfig({
+  site: site,
   vite: {
     plugins: [tailwindcss(), yaml()],
   },
-
   integrations: [
     alpinejs({ entrypoint: "/src/scripts/alpine/index.ts" }),
     icon(),
+    sitemap(),
   ],
 });
