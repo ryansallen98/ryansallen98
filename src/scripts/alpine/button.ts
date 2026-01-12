0c5019850d6977302fbe6ab152d2236fa0b6c99b
Ryan Allen <ryansallen53@gmail.com>
2026-01-12 14:05:49 +0000

    feat: implement IconButton component with hover effects and animations

--- src/scripts/alpine/button.ts
+++ src/scripts/alpine/button.ts
@@ -9,15 +9,28 @@ export function buttonHover() {
 
       const initialText = this.$refs.initialText;
       const hoverText = this.$refs.hoverText;
+      const initialIcon = this.$refs.initialIcon;
+      const hoverIcon = this.$refs.hoverIcon;
       const initialIconStart = this.$refs.initialIconStart;
       const initialIconEnd = this.$refs.initialIconEnd;
       const hoverIconStart = this.$refs.hoverIconStart;
       const hoverIconEnd = this.$refs.hoverIconEnd;
 
-      initialText.style.transform = "translateY(-100%)";
-      hoverText.style.transform = "translateY(0)";
-      hoverText.style.opacity = "1";
-      hoverText.style.filter = "blur(0)";
+      if (initialText && hoverText) {
+        initialText.style.transform = "translateY(-100%)";
+        hoverText.style.transform = "translateY(0)";
+        hoverText.style.opacity = "1";
+        hoverText.style.filter = "blur(0)";
+      }
+
+      if (initialIcon && hoverIcon) {
+        initialIcon.style.transform = "translateY(-100%)";        
+        initialIcon.style.opacity = "0";
+        initialIcon.style.filter = "blur(5px)";
+        hoverIcon.style.transform = "translateY(0)";
+        hoverIcon.style.opacity = "1";
+        hoverIcon.style.filter = "blur(0)";
+      }
 
       if (initialIconStart && hoverIconStart) {
         initialIconStart.style.transform = "translateX(-100%)";
@@ -39,15 +52,28 @@ export function buttonHover() {
 
       const initialText = this.$refs.initialText;
       const hoverText = this.$refs.hoverText;
+      const initialIcon = this.$refs.initialIcon;
+      const hoverIcon = this.$refs.hoverIcon;
       const initialIconStart = this.$refs.initialIconStart;
       const initialIconEnd = this.$refs.initialIconEnd;
       const hoverIconStart = this.$refs.hoverIconStart;
       const hoverIconEnd = this.$refs.hoverIconEnd;
 
-      initialText.style.transform = "translateY(0)";
-      hoverText.style.transform = "translateY(100%)";
-      hoverText.style.opacity = "0";
-      hoverText.style.filter = "blur(5px)";
+      if (initialText && hoverText) {
+        initialText.style.transform = "translateY(0)";
+        hoverText.style.transform = "translateY(100%)";
+        hoverText.style.opacity = "0";
+        hoverText.style.filter = "blur(5px)";
+      }
+
+      if (initialIcon && hoverIcon) {
+        initialIcon.style.transform = "translateY(0%)";
+        initialIcon.style.opacity = "1";
+        initialIcon.style.filter = "blur(0)";
+        hoverIcon.style.transform = "translateY(100%)";
+        hoverIcon.style.opacity = "0";
+        hoverIcon.style.filter = "blur(5px)";
+      }
 
       if (initialIconStart && hoverIconStart) {
         initialIconStart.style.transform = "translateX(0)";
